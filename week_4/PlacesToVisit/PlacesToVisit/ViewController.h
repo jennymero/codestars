e246cea09417f4f8ff912eccef9ccc1fb8fb8f91
jennymero <jenny@jennymero.com>
2015-08-30 17:34:59 -0400

    Calculator app, Places app with deletion capabilities, AppCoda Chapters 4-6 Code Along

---
+++ week_4/PlacesToVisit/PlacesToVisit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlacesToVisit
+//
+//  Created by JENNY MERO on 8/26/15.
+//  Copyright (c) 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
