c05254712c08f58f6ee44c3ceefa9e2082f06181
jennymero <jenny@jennymero.com>
2015-09-02 16:22:17 -0400

    App with tabs and webview

---
+++ week_5/AppCoda_Chapter_7_8_9_10_11/RecipeApp/CustomTable/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  CustomTable
+//
+//  Created by JENNY MERO on 9/2/15.
+//  Copyright (c) 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+
+@end
