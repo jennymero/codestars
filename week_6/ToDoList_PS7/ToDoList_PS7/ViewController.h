f9c43d73775c54b0909aaef62aadb875b75c3747
jennymero <jenny@jennymero.com>
2015-09-15 23:56:12 -0400

    Beginning of To Do List app. Has add and save buttons

---
+++ week_6/ToDoList_PS7/ToDoList_PS7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList_PS7
+//
+//  Created by JENNY MERO on 9/14/15.
+//  Copyright (c) 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ week_6/ToDoList_PS7/ToDoList_PS7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList_PS7
+//
+//  Created by JENNY MERO on 9/14/15.
+//  Copyright (c) 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
