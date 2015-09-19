07f4c3552e5204fc90b7e7a80ae2c01801b5712a
jennymero <jenny@jennymero.com>
2015-09-19 14:07:46 -0400

    working tableview no images yet

---
+++ week_7/Artisanal1/CustomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewController.h
+//  Artisanal1
+//
+//  Created by JENNY MERO on 9/19/15.
+//  Copyright © 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewController : UITableViewController
+
+@end
