3868d757dc1d595f241fab5b78a5f2aee30b3ae2
jennymero <jenny@jennymero.com>
2015-10-04 21:53:54 -0400

    Playing with static tables

---
+++ week_8/StaticTableDemo/StaticTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticTableDemo
+//
+//  Created by JENNY MERO on 10/4/15.
+//  Copyright © 2015 Jenny Mero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
